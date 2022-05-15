c3996f02a65ba610d7f8eae71062d5ddf4185b89
dstuthi76 <74001518+dstuthi76@users.noreply.github.com>
2022-05-15 06:17:03 +0530

    Add files via upload

---
+++ a.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import React from "react";
+import Categories from "../components/Categories1";
+import Footer from "../components/footer1";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+import Slider from "../components/Slider";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const a = () => {
+  return  (
+  <div>
+     
+  </div>
+
+  )
+};
+
+export default a;
